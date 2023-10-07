c1dcfd173eda45c64c0167d5f97859b5fe8624d1
miracle485 <15954217910@163.com>
2023-10-07 19:19:21 +0800

    调整生产者生成方式

--- src/main/java/top/magic/rocketmqagent/service/ProduceBuilder.java
+++ src/main/java/top/magic/rocketmqagent/service/ProduceBuilder.java
@@ -2,6 +2,8 @@ package top.magic.rocketmqagent.service;
 
 import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.rocketmq.acl.common.AclClientRPCHook;
+import org.apache.rocketmq.acl.common.SessionCredentials;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.springframework.stereotype.Service;
@@ -26,11 +28,18 @@ public class ProduceBuilder {
 
 
     private synchronized DefaultMQProducer generateMQProducer(String url, String ak, String sk) throws MQClientException {
-        DefaultMQProducer mqProducer = new DefaultMQProducer();
+        DefaultMQProducer mqProducer = createDefaultProducer(ak, sk);
         mqProducer.setNamesrvAddr(url);
 
         mqProducer.start();
         producerMap.put(url, mqProducer);
         return mqProducer;
+    }
+
+    private DefaultMQProducer createDefaultProducer(String ak, String sk) {
+        if (StringUtils.isBlank(ak) || StringUtils.isBlank(sk)) {
+            return new DefaultMQProducer();
+        }
+        return new DefaultMQProducer(new AclClientRPCHook(new SessionCredentials(ak, sk)));
     }
 }

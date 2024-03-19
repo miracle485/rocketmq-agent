7504081ec68e20ff5f1092b6770b87fc862d6278
wangyilin29 <wangyilin29@meituan.com>
2024-03-19 19:20:55 +0800

    [feature] 完善配置

--- src/main/java/top/magic/rocketmqagent/service/auto/RocketMqAgentAutoLoad.java
+++ src/main/java/top/magic/rocketmqagent/service/auto/RocketMqAgentAutoLoad.java
@@ -1,17 +1,15 @@
 package top.magic.rocketmqagent.service.auto;
 
-import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.magic.rocketmqagent.model.config.AgentConfig;
-import top.magic.rocketmqagent.model.file.FileSizeEnums;
 import top.magic.rocketmqagent.service.store.MsgFileQueue;
 
 @Configuration
-public class BeanAutoLoad {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BeanAutoLoad.class);
+public class RocketMqAgentAutoLoad {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RocketMqAgentAutoLoad.class);
 
     @Bean
     public MsgFileQueue getMsgFile(AgentConfig agentConfig) {
